aa4b739f49712a4defe263fe2086dbad7fd8bf49
Harrison2301 <harrisonwinters2301@gmail.com>
2020-04-05 18:56:25 +0000

    get weather api call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,18 @@
 const key = 'UtRMbYoStmXijPEhaPZkhmWLpaTa251q';
 
+// get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0]
+}
+
+// get city information
 const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
     const query = `?apikey=${key}&q=${city}`;
@@ -10,6 +23,9 @@ const getCity = async (city) => {
     return data[0];
 };
 
-getCity('Manchester')
-.then(data => console.log(data))
+getCity('Manchester').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data)
+})
 .catch(err => console.log(err))
